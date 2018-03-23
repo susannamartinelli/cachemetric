7bf7d5fd1a0023ac0ce26127ec8d8f644fa54614
Susanna Martinelli <susanna.martinelli@geouniq.com>
2018-03-23 17:36:35 +0100

    first commit

---
+++ kong/plugins/access.lua
@@ -0,0 +1,13 @@
+require "resty.core.shdict"
+
+local kongcache = ngx.shared.kong_cache
+
+local _M = {}
+
+
+
+function _M.execute(conf)
+    ngx.log(ngx.ERR, "kong cache:")
+    ngx.log(ngx.ERR, kongcache:capacity())
+    ngx.log(ngx.ERR, kongcache:free_space()
+end
